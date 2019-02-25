7cccd09965180bbc5227a01f79a8afa9bdfdb8d8
MikiKru <michal_kruczkowski@o2.pl>
2019-02-25 15:51:33 +0100

    Project final

--- src/controller/DetailCourseController.java
+++ src/controller/DetailCourseController.java
@@ -1,11 +1,14 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.Participant;
 
 public class DetailCourseController {
@@ -42,20 +45,44 @@ public class DetailCourseController {
 
     @FXML
     void deleteParticipant(ActionEvent event) {
-
+        Participant p_deleted = tbl_particip.getSelectionModel().getSelectedItem();
+        CourseController.c_selected.deleteParticipant(p_deleted);
+        participants.remove(p_deleted);
+        lbl_course_particip.setText(String.valueOf(participants.size()));
+        insertParticipanstIntoTableView();
     }
 
+    // ----------------------------------------------------------------------------
+    ObservableList<Participant> participants = FXCollections.observableArrayList();
+    // --------------metoda wpisująca uczestników na listę ------------------------
+    private void insertParticipanstIntoTableView(){
+        col_particip_name.setCellValueFactory(new PropertyValueFactory<Participant, String>("particip_name"));
+        col_particip_lastname.setCellValueFactory(new PropertyValueFactory<Participant, String>("particip_lastname"));
+        tbl_particip.setItems(participants);
+    }
     @FXML
     void saveParticipant(ActionEvent event) {
-
+        String particip_name = tf_particip_name.getText();
+        String particip_lastname = tf_particip_lastname.getText();
+        Participant p = new Participant(particip_name, particip_lastname);
+        CourseController.c_selected.addParticipant(p);
+        participants.add(p);
+        // wpisanie danych do tabelki
+        insertParticipanstIntoTableView();
+        // aktualizacja liczby uczestników
+        lbl_course_particip.setText(String.valueOf(CourseController.c_selected.getNoParticipants()));
+        tf_particip_lastname.clear();
+        tf_particip_name.clear();
     }
     public void initialize(){
+        participants.addAll(CourseController.c_selected.getCourse_participants());
+        insertParticipanstIntoTableView();
         // ustawienie danych wybranego kursu
         lbl_course_name.setText(CourseController.c_selected.getCourse_name());
         lbl_course_category.setText(CourseController.c_selected.getCourse_category());
         lbl_course_date.setText(CourseController.c_selected.getCourse_date().toString());
         lbl_course_trainer.setText(CourseController.c_selected.getCourse_trainer());
-        lbl_course_particip.setText(CourseController.c_selected.getNoParticipants());
+        lbl_course_particip.setText(String.valueOf(CourseController.c_selected.getNoParticipants()));
     }
 
 }
--- src/model/Participant.java
+++ src/model/Participant.java
@@ -1,22 +1,12 @@
 package model;
 
 public class Participant {
-    private int particip_id;
     private String particip_name;
     private String particip_lastname;
 
-    public Participant(int particip_id, String particip_name, String particip_lastname) {
-        this.particip_id = particip_id;
+    public Participant(String particip_name, String particip_lastname) {
         this.particip_name = particip_name;
         this.particip_lastname = particip_lastname;
-    }
-
-    public int getParticip_id() {
-        return particip_id;
-    }
-
-    public void setParticip_id(int particip_id) {
-        this.particip_id = particip_id;
     }
 
     public String getParticip_name() {
